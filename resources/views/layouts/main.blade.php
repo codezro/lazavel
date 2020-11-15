a62d93404d17694ea7031f085a3e5a6cc386983d
codezroo <chevbanico@gmail.com>
2020-11-15 14:32:27 +0800

    logo fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -50,7 +50,7 @@
         </nav>
         <nav class="bottom">
             <div class="row">
-                <div class="col s1 offset-s1 padding-top-10"><img src="/resources/images/lazavel2.PNG" class="logo"/></div>
+                <div class="col s1 offset-s1 padding-top-10"><img src="resources/images/lazavel2.PNG" class="logo"/></div>
                 <div class="col s7 row offset-s3 padding-top-15"><input class="col s8 search-bar"/><div class="bg-o col s1 search-icon"><i class="white-text large material-icons">search</i></div></div>
             </div>
         </nav>
